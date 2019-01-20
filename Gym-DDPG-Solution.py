1e90a1c9acc96cba67089662c0529fa8b6c204b9
Shu <laozishixs@gmail.com>
2019-01-20 12:23:09 +0100

    Add files via upload

--- Gym-DDPG-Solution.py
+++ Gym-DDPG-Solution.py
@@ -19,8 +19,6 @@ print(o_space)
 print(zip(env.observation_space.low, env.observation_space.high))
 
 
-import numpy as np
-
 class Memory():
     # class to store x/u trajectory
     def __init__(self, buffer_shapes, buffer_size=int(1e5)):
@@ -45,57 +43,11 @@ class Memory():
         return batch
 
 
-
-
-# Q network (also the critic network in DDPG)
-class QFunction:
-    def __init__(self, o_space, a_space, gamma=0.99, scope='Q'):
+class DDPGFunction:
+    def __init__(self, o_space, a_space, gamma=0.99, scope='', mode='Q'):
         self._scope = scope
         self._o_space = o_space
         self._a_space = a_space
-        self._action_n = 7 #self._a_space.n
-        self._gamma = gamma
-        self._sess = tf.get_default_session() or tf.InteractiveSession()
-    
-        with tf.variable_scope(self._scope, reuse=tf.AUTO_REUSE):
-            self._build_graph()
-    
-    def _build_graph(self):
-        self.state = tf.placeholder(dtype=tf.float32, shape=(None, self._o_space.shape[0]), name="state")
-        
-        dense1 = tf.layers.dense(inputs=self.state, units=100, activation=tf.nn.leaky_relu, name='dense1')
-        dense2 = tf.layers.dense(inputs=dense1, units=100, activation=tf.nn.leaky_relu, name='dense2')
-        self.h = tf.layers.dense(inputs=dense2, units=7, activation=None, name='h') #nx7
-        self.output = tf.squeeze(self.h, name='pred')
-        
-        tf.add_to_collection('pred_Qs', self.output, scope=self.scope)
-        #tf.add_to_collection('state', self.state, scope=scope) #however not used in the documents' examples
-        
-    
-    def Qs(self, state):
-        _state = np.asarray(state).reshape(-1, self._o_space.shape[0])
-        inp = {self.state: _state}
-        return self._sess.run(self.output, feed_dict=inp).reshape(-1, self._action_n)
-    
-    def Q(self, state, action):
-        _action = (action,) if type(action) == int else action
-        return self.Qs(state)[(tuple(range(len(_action))), _action)]
-    
-    def V(self, state):
-        V = np.max(self.Qs(state), axis=1)
-        return np.squeeze(V)
-    
-    def save(self, path='./DQN_model.ckpt'):
-        saver = tf.train.Saver()
-        
-        saver.save(self._sess, path)
-        print ('model saved under the path: ', path)
-
-
-class DDPGFunction:
-    def __init__(self, o_space, gamma=0.99, scope='Actor', mode='Q'):
-        self._scope = scope
-        self._o_space = o_space
         self._action_n = 3 #self._a_space.n
         self._gamma = gamma
         self._sess = tf.get_default_session() or tf.InteractiveSession()
@@ -106,14 +58,26 @@ class DDPGFunction:
         else:
             with tf.variable_scope(self._scope, reuse=tf.AUTO_REUSE): #for the 2 actor networks
                 self._build_graph_mu()
-            
-    def _build_graph_q(self):
-        self.state = tf.placeholder(dtype=tf.float32, shape=(None, self._action_n), name="state")
-        self.action = tf.placeholder(dtype=tf.float32, shape=(None, self._o_space.shape[0]), name="state")
-        dense1 = tf.layers.dense(inputs=self.state, units=100, activation=tf.nn.relu, name='l_state')  #nx100
-        dense2 = tf.layers.dense(inputs=self.action, units=100, activation=tf.nn.relu, name='l_action') #nx100
-        sa = tf.add(dense1,dense2) #nx100
-        self.h = tf.layers.dense(inputs=sa, units=1, activation=None, name='h') #nx1
+                
+        
+        
+    def _build_graph_q(self):  
+        self.state = tf.placeholder(dtype=tf.float32, shape=(None, self._o_space.shape[0]), name="state") #nx16
+        self.action = tf.placeholder(dtype=tf.float32, shape=(None, self._action_n), name="action") #nx3
+#        dense1 = tf.layers.dense(inputs=self.state, units=100, activation=tf.nn.relu, name='l_state')  #nx100
+#        dense2 = tf.layers.dense(inputs=self.action, units=100, activation=tf.nn.relu, name='l_action') #nx100
+#        
+#        sa = tf.add(dense1,dense2) #nx100
+#        self.h = tf.layers.dense(inputs=sa, units=1, activation=None, name='h') #nx1
+#        self.output = tf.squeeze(self.h, name='predQ')
+        
+        init_w = tf.random_normal_initializer(0., 0.1)
+        init_b = tf.constant_initializer(0.1)
+        w1_s = tf.get_variable('w1_s', [self._o_space.shape[0], 100], trainable=1)
+        w1_a = tf.get_variable('w1_a', [self._action_n, 100], trainable=1)
+        b1 = tf.get_variable('b1', [1, 100], trainable=1)
+        net = tf.nn.relu(tf.matmul(self.state, w1_s) + tf.matmul(self.action, w1_a) + b1)
+        self.h = tf.layers.dense(net, 1, trainable=1)
         self.output = tf.squeeze(self.h, name='predQ')
         
     def _build_graph_mu(self):
@@ -128,7 +92,7 @@ class DDPGFunction:
         inp = {self.state: _state}
         return self._sess.run(self.output, feed_dict=inp).reshape(-1, self._action_n)
     
-    def Q(self, state, action): #output Q for the loss
+    def Qs(self, state, action): #output Q for the loss
         _state = np.asarray(state).reshape(-1, self._o_space.shape[0])
         _action = np.asarray(action).reshape(-1, self._action_n)
         inp = {self.state: _state, self.action: _action}
@@ -142,127 +106,6 @@ class DDPGFunction:
             
 # In[2]: define Agents 
 
-
-class DQNAgent(object):
-    """
-    Agent implementing Q-learning with NN function approximation.
-    """
-    def __init__(self, o_space, a_space, scope='DQNAgent', **userconfig):
-        
-        self._o_space = o_space
-        self._a_space = a_space
-        self._action_n = 7 #a_space.n
-        self._config = {
-            "eps_begin": 0.3,            # Epsilon in epsilon greedy policies
-            "eps_end": 0.05,
-            "eps_decay": 0.99,
-            "discount": 0.95,
-            "buffer_size": int(5e5),
-            "batch_size": 32,
-            "learning_rate": 1e-5,
-            "theta": 0.05,
-            "use_target_net": True,}
-        self._config.update(userconfig)
-        self._scope = scope
-        self._eps = self._config['eps_begin']
-        self._buffer_shapes = {
-            's': self._o_space.shape[0],
-            'a': None,
-            'r': None,
-            's_prime': self._o_space.shape[0],
-            'd': None,
-        }
-        self._buffer = Memory(buffer_shapes=self._buffer_shapes, buffer_size=self._config['buffer_size'])
-        self._sess = tf.get_default_session() or tf.InteractiveSession()
-        
-        # Create Q Networks
-        with tf.variable_scope(self._scope, reuse=tf.AUTO_REUSE):
-            self._Q = QFunction(scope='Q', o_space=self._o_space, a_space=self._a_space, 
-                                         gamma=self._config['discount'])
-            if self._config['use_target_net']:
-                self._Q_target = QFunction(scope='Q_target', o_space=self._o_space, a_space=self._a_space, 
-                                             gamma=self._config['discount'])
-                
-            self._prep_train()
-            
-        self._sess.run(tf.global_variables_initializer())
-        
-            
-    def _vars(self, scope=''):
-        return tf.get_collection(tf.GraphKeys.TRAINABLE_VARIABLES, scope=self._scope + '/' + scope)
-    
-    def _global_vars(self, scope=''):
-        return tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=self._scope + '/' + scope)
-            
-    def _prep_train(self):
-        self._action = tf.placeholder(dtype=tf.int32, shape=(None,), name="action") #??
-        self._action_onehot = tf.one_hot(self._action, self._action_n, dtype=tf.float32)
-        self._Qval = tf.reduce_sum(tf.multiply(self._Q.output, self._action_onehot), axis=1)
-        self._target = tf.placeholder(dtype=tf.float32, shape=(None,), name="target")
-        self._loss = tf.reduce_mean(tf.square(self._Qval - self._target))
-        self._optim = tf.train.AdamOptimizer(learning_rate=self._config['learning_rate'])
-        self._train_op = self._optim.minimize(self._loss)
-        
-        if self._config['use_target_net']:
-            self._update_target_op = [tf.assign(
-                target_var, 
-                (1-self._config['theta'])*target_var+self._config['theta']*Q_var) 
-                                     for target_var, Q_var in zip(self._vars('Q_target'), self._vars('Q'))]
-        
-    def _update_target_net(self):
-        self._sess.run(self._update_target_op)
-
-    def act(self, observation, eps=None):
-        if eps is None:
-            eps = self._eps
-        # epsilon greedy.
-        if np.random.random() > eps:
-            action = np.argmax(self._Q.Qs(observation)) 
-            #action = env.discrete_to_continous_action(action)
-            #action = np.hstack([action, [0,0.,0]])
-        else: 
-            action = np.random.randint(0, 6)
-            #action = self._a_space.sample()        
-        return action
-    
-    def store_transition(self, transition):
-        self._buffer.add_item(transition)
-        
-    def _eps_scheduler(self, writer=None, ep=0):
-        self._eps = max(self._config['eps_end'], self._eps*self._config['eps_decay'])
-        if writer: writer.add_scalar('policy/eps', self._eps, ep)
-    
-    def train(self, iter_fit=10, writer=None, ep=0):
-        losses = []
-        for i in range(iter_fit):
-
-            # sample from the replay buffer
-            data = self._buffer.sample(batch_size=self._config['batch_size'])
-            
-            s = data['s'] # s_t
-            a = data['a'] # a_t
-            r = data['r'] # rew
-            s_prime = data['s_prime'] # s_t+1
-            d = data['d'] # done
-            if self._config['use_target_net']:
-                v_prime = self._Q_target.V(s_prime)
-            else:
-                v_prime = self._Q.V(s_prime)    
-
-            # target
-            td_target = r + self._config['discount'] * v_prime * (1-d)
-
-            # optimize the lsq objective
-            inp = {self._Q.state: s, self._action: a, self._target: td_target}
-            fit_loss = self._sess.run([self._train_op, self._loss], feed_dict=inp)[1]
-            losses.append(fit_loss)
-            
-            if self._config['use_target_net']:
-                self._update_target_net()
-            
-        return losses
-    
-    
 
 
 # In[3] DDPG-Agent
@@ -282,7 +125,8 @@ class DDPGAgent(object):
             "discount": 0.95,
             "buffer_size": int(5e5),
             "batch_size": 32,
-            "learning_rate": 1e-5,
+            "learning_rate_a": 1e-5,
+            "learning_rate_c": 2e-5,
             "theta": 0.05,
             "use_target_net": True,}
         self._config.update(userconfig)
@@ -302,7 +146,7 @@ class DDPGAgent(object):
         with tf.variable_scope(self._scope, reuse=tf.AUTO_REUSE):
             self._Q = DDPGFunction(scope='Q', o_space=self._o_space, a_space=self._a_space, 
                                          gamma=self._config['discount'], mode='Q')
-            self._Q_target = QFunction(scope='Q_target', o_space=self._o_space, a_space=self._a_space, 
+            self._Q_target = DDPGFunction(scope='Q_target', o_space=self._o_space, a_space=self._a_space, 
                                              gamma=self._config['discount'], mode='Q')
             self._Mu = DDPGFunction(scope='Mu', o_space=self._o_space, a_space=self._a_space, 
                                          gamma=self._config['discount'], mode='Mu')
@@ -318,24 +162,96 @@ class DDPGAgent(object):
     
     def _global_vars(self, scope=''):
         return tf.get_collection(tf.GraphKeys.GLOBAL_VARIABLES, scope=self._scope + '/' + scope)
+    
+    def store_transition(self, transition):
+        self._buffer.add_item(transition)
+        
+    # define the loss functions here
+    def _prep_train(self):
+        #self._action = tf.placeholder(dtype=tf.int32, shape=(None,), name="action") #??
+        #self._action_onehot = tf.one_hot(self._action, self._action_n, dtype=tf.float32)
+        self._Qval = self._Q.output #nx1
+        # critic
+        self._target = tf.placeholder(dtype=tf.float32, shape=(None,), name="target")
+        self._c_loss = tf.reduce_mean(tf.square(self._Qval - self._target))
+        self._c_optim = tf.train.AdamOptimizer(learning_rate=self._config['learning_rate_c'])
+        self._c_train_op = self._c_optim.minimize(self._c_loss)
+        
+        # actor
+        self._a_loss = - tf.reduce_mean(self._Q.output)
+        self._a_optim = tf.train.AdamOptimizer(learning_rate=self._config['learning_rate_a'])
+        self._a_train_op = self._a_optim.minimize(self._a_loss)
+        
+        self._q_update_target_op = [tf.assign(
+                target_var, 
+                (1-self._config['theta'])*target_var + self._config['theta']*eval_var) 
+                                     for target_var, eval_var in zip(self._vars('Q_target'), self._vars('Q'))]
+        self._mu_update_target_op = [tf.assign(
+                target_var, 
+                (1-self._config['theta'])*target_var + self._config['theta']*eval_var) 
+                                     for target_var, eval_var in zip(self._vars('Mu_target'), self._vars('Mu'))]
+            
+    def train(self, iter_fit=4, writer=None, ep=0):
+        losses = []
+        for i in range(iter_fit):
 
+            # sample from the replay buffer
+            data = self._buffer.sample(batch_size=self._config['batch_size'])
+            
+            s = data['s'] # s_t
+            a = data['a'] # a_t  ###############################ATTENTION: NOW nx3 instead of nx1
+            r = data['r'] # rew
+            s_prime = data['s_prime'] # s_t+1
+            d = data['d'] # done
+            
+            As = self._Mu_target.As(s_prime) # + N ?
+            y_i = r + self._config['discount'] * self._Q_target.Qs(s_prime, As) # * d
+            
+
+            # optimize the lsq objective
+            # critic
+            inp = {self._Q.state: s, self._Q.action: a, self._target: y_i}
+            fit_loss = self._sess.run([self._c_train_op, self._c_loss], feed_dict=inp)[1]
+            # actor
+            a_loss = self._sess.run([self._a_train_op, self._a_loss], feed_dict=inp)[1]
+            losses.extend([fit_loss, a_loss])
+        if self._config['use_target_net']:
+            self._q_update_target_net()
+            self._mu_update_target_net()
+            
+        return losses
+    
+    def act(self, observation, eps=None):
+#        if eps is None:
+#            eps = self._eps
+#        # epsilon greedy.
+#        if np.random.random() > eps:
+#            action = np.argmax(self._Q.Qs(observation)) 
+#            #action = env.discrete_to_continous_action(action)
+#            #action = np.hstack([action, [0,0.,0]])
+#        else: 
+#            action = np.random.randint(0, 6)
+#            #action = self._a_space.sample()        
+        return self._Q_target.As(observation)
     
 # In[4]: Initializing training parameters
 
 
 fps = 100 # env.metadata.get('video.frames_per_second')
-max_steps = 250 #env.spec.tags['wrapper_config.TimeLimit.max_episode_steps']
+max_steps = 80 #env.spec.tags['wrapper_config.TimeLimit.max_episode_steps']
 #max_steps
 
 
 # In[4]: Start training
 
 
-q_agent = DQNAgent(o_space, ac_space, discount=0.99, eps_begin=0.3)
+ddpg_agent = DDPGAgent(o_space, ac_space, discount=0.99, eps_begin=0.3)
 
+# test the outputs
+ac_output = ddpg_agent._Mu.As(ob)
+q_output = ddpg_agent._Q.Qs(ob, ac_output)
 
-
-
+# start training
 stats = []
 losses = []
 
